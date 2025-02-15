497f85b5592b5113b34cae88efcea3e262265ee4
Miguel <guelferreirar@gmail.com>
2025-02-15 11:49:46 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,30 +2,19 @@
 
 LOG_FILE="/var/log/setup.log"
 
-# Atualiza os pacotes
-sudo yum update -y | tee -a $LOG_FILE
+yum update -y | tee -a $LOG_FILE
 
-# Instala Apache (httpd), Git e Curl
-sudo yum install -y httpd git curl | tee -a $LOG_FILE
+yum install -y httpd | tee -a $LOG_FILE
+systemctl start httpd | tee -a $LOG_FILE
+systemctl enable httpd | tee -a $LOG_FILE
 
-# Inicia e habilita o Apache (httpd)
-sudo systemctl start httpd | tee -a $LOG_FILE
-sudo systemctl enable httpd | tee -a $LOG_FILE
-
-# Clona o repositório com o site
+yum install -y git | tee -a $LOG_FILE
 git clone https://github.com/MiguelMFR/trabalho-aws-index.git /var/www/html/ | tee -a $LOG_FILE
 
-# Verifica se os arquivos foram baixados corretamente
-ls -lah /var/www/html/ | tee -a $LOG_FILE
+chown -R apache:apache /var/www/html | tee -a $LOG_FILE
 
-# Define permissões para o Apache
-sudo chown -R apache:apache /var/www/html/
-sudo chmod -R 755 /var/www/html/
-
-# Aguarda um tempo para o Apache iniciar corretamente
-sleep 15
-
-# Envia solicitação POST e exibe detalhes para depuração
-curl -X POST -d "nome=Miguel Ferreira" https://difusaotech.com.br/lab/aws/index.php --verbose | tee -a $LOG_FILE
+curl -X POST -d "nome=Miguel Ferreira" https://difusaotech.com.br/lab/aws/index.php
 
 echo "Setup concluído!" | tee -a $LOG_FILE
+
+
